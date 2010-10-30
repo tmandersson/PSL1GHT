757a819867692ccda559525771fcad1f7b2708b5
Aaron Lindsay <aaron@aaronlindsay.com>
2010-10-30 13:51:53 -0400

    SPRX linking support; apps no longer crash on exit

---
+++ psl1ght/sprx/liblv2/include/config.h
@@ -0,0 +1,6 @@
+#define LIBRARY_NAME "sysPrxForUser"
+#define LIBRARY_NAME_PADDED "sysPrxForUser\0\0"
+#define LIBRARY_SYMBOL sysPrxForUser
+
+#define LIBRARY_HEADER_1	0x2c000001
+#define LIBRARY_HEADER_2	0x00090001 // Count of exports

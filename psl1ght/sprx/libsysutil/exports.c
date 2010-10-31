72684f2c6d649992e31ef10cc2e196596e58013f
Scott Mansell <phiren@gmail.com>
2010-11-01 12:57:02 +1300

    Added libsysutil

---
+++ psl1ght/sprx/libsysutil/exports.c
@@ -0,0 +1,5 @@
+EXPORT(videoGetResolution, 0xe558748d);
+EXPORT(videoConfigure, 0x0bae8772);
+EXPORT(videoGetState, 0x887572d5);
+EXPORT(SysutilRegisterCallback, 0x9d98afa0);
+EXPORT(SysutilCheckCallback, 0x189a74da);

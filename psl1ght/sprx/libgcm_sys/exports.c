dd534437a5e143ddf083378cd40215aa8ea0e67e
Scott Mansell <phiren@gmail.com>
2010-11-01 14:16:00 +1300

    Added minimal libgcm_sys stub

---
+++ psl1ght/sprx/libgcm_sys/exports.c
@@ -0,0 +1,12 @@
+EXPORT(GcmInitBody, 0x15bae46b);
+EXPORT(GcmAddressToOffset, 0x2a6fba9c);
+EXPORT(GcmFunc15, 0x3a33c1fd);
+EXPORT(GcmSetFlipMode, 0x4ae8d215);
+EXPORT(GcmGetFlipStatus, 0x72a577ce);
+EXPORT(GcmSetWaitFlip, 0x983fb9aa);
+EXPORT(GcmSetDisplayBuffer, 0xa53d12ae);
+EXPORT(GcmGetControlRegister, 0xa547adde);
+EXPORT(GcmResetFlipStatus, 0xb2e761d4);
+EXPORT(GcmSetFlip, 0xdc09357e);
+EXPORT(GcmGetConfiguration, 0xe315a0b2);
+

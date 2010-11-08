e2267d610e759a24eba1bf4b47e3c75722f0e42d
Scott Mansell <phiren@gmail.com>
2010-11-08 22:41:58 +1300

    I think I've written enough to get the rsx doing a clear. Now I think i could do with some food eat.

--- psl1ght/lib/libreality/source/buffer.c
+++ psl1ght/lib/libreality/source/buffer.c
@@ -4,8 +4,8 @@
 int checkCommandBufferLength(gcmContextData *context, uint32_t len) {
 	if (context->current + len > context->end) {
 		int retval; // It's a bit messy, but it works.
-		int tocval = ((uint32_t *)(uint64_t) context->callback)[1];
-		int procaddr = ((uint32_t *)(uint64_t) context->callback)[0];
+		const int tocval = ((uint32_t *)(uint64_t) context->callback)[1];
+		const int procaddr = ((uint32_t *)(uint64_t) context->callback)[0];
 		asm("mr 3,%3; mr 31,2; mr 2,%1; mtctr %2; bctrl; mr 2,31; mr %0,3;" : "=r"(retval) : "r"(tocval), "r"(procaddr), "r"(context) : "r31", "r3");
 		return retval;
 	}
--- psl1ght/lib/libreality/source/commands.c
+++ psl1ght/lib/libreality/source/commands.c
@@ -17,15 +17,37 @@ void realityClearBuffers(gcmContextData *context, uint32_t buffers) {
 	commandBufferPutCmd1(context, NV30_3D_CLEAR_BUFFERS, buffers);
 }	
 
-void realitySetDmaLocation(gcmContextData *context, uint8_t color0, uint8_t color1, uint8_t color2, uint8_t color3, uint8_t zeta) {
-	COMMAND_LENGTH(context, 0x20);
-	// Setup DMA handles
-	commandBufferPutCmd1(context, NV30_3D_DMA_COLOR1, color1 | 0xFEED0000);
-	commandBufferPutCmd2(context, NV30_3D_DMA_COLOR0, 0xFEED0000 | color0, 0xFEED0000 | zeta);
-	commandBufferPutCmd2(context, NV40_3D_DMA_COLOR2, 0xFEED0000 | color2, 0xFEED0000 | color3);
+const static uint32_t offset_cmds[] = {NV30_3D_COLOR0_OFFSET, NV30_3D_COLOR1_OFFSET,
+		NV40_3D_COLOR2_OFFSET, NV40_3D_COLOR3_OFFSET, NV30_3D_ZETA_OFFSET};
+const static uint32_t dma_cmds[] = {NV30_3D_DMA_COLOR0, NV30_3D_DMA_COLOR1,
+		NV40_3D_DMA_COLOR2, NV40_3D_DMA_COLOR3, NV30_3D_DMA_ZETA};
+const static uint32_t pitch_cmds[] = {NV30_3D_COLOR0_PITCH, NV30_3D_COLOR1_PITCH,
+		NV40_3D_COLOR2_PITCH, NV40_3D_COLOR3_PITCH, NV40_3D_ZETA_PITCH};
+
+void realitySetupRenderSurface(gcmContextData *context, uint8_t surface, uint8_t location,
+				uint32_t offset, uint32_t pitch) {
+	COMMAND_LENGTH(context, 0x18);
+	commandBufferPutCmd1(context, dma_cmds[surface], location | 0xFEED0000);
+	commandBufferPutCmd1(context, offset_cmds[surface], offset);
+	commandBufferPutCmd1(context, pitch_cmds[surface], pitch);
 }
 
-void realitySetViewportOffset(gcmContextData *context, uint16_t x, uint16_t y) {
+void realitySelectRenderTarget(gcmContextData *context, uint8_t target, uint32_t format, 
+				uint16_t width, uint16_t height, uint16_t x, uint16_t y) {
+	COMMAND_LENGTH(context, 0x2c);
+
+	format |= (31 - __builtin_clz(width)) << 16;
+	format |= (31 - __builtin_clz(height)) << 24;
+	uint32_t conventions = height | 0x1000; // COORD_CONVENTIONS_ORIGIN_INVERTED
+	
+	commandBufferPutCmd1(context, NV30_3D_RT_FORMAT, format); 
+	commandBufferPutCmd1(context, NV30_3D_RT_ENABLE, target);
+	commandBufferPutCmd1(context, NV30_3D_VIEWPORT_TX_ORIGIN, x | y << 16);
+	commandBufferPutCmd1(context, NV30_3D_COORD_CONVENTIONS, conventions);
+	commandBufferPutCmd2(context, NV30_3D_RT_HORIZ, x | width << 16, y | height << 16);
+}
+
+void realitySetViewportOffset(gcmContextData *context, uint16_t width, uint16_t height, uint16_t x, uint16_t y) {
 	COMMAND_LENGTH(context, 0x8);
 	commandBufferPutCmd1(context, NV30_3D_VIEWPORT_TX_ORIGIN, x | y << 16);
 }

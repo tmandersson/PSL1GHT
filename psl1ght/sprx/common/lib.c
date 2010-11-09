79b60c3f0d880be7fff69c2b871b55e6edad51f4
Aaron Lindsay <aaron@aaronlindsay.com>
2010-11-09 15:56:14 -0500

    Fixed SPRX linking issue with large binaries.

--- psl1ght/sprx/common/lib.c
+++ psl1ght/sprx/common/lib.c
@@ -18,7 +18,7 @@ typedef struct {
 } __attribute__((__packed__)) prx_header;
 
 extern uint32_t LIBRARY_SYMBOL __attribute__((section(".rodata.sceFNID")));
-static const void* scefstub[0] __attribute__((section(".data.sceFStub." LIBRARY_NAME)));
+static const void* scefstub[0] __attribute__((section(".data.sceFStub")));
 
 static const uint32_t version __attribute__((section(".rodata.sceResident"))) = 0;
 static const const char name[] __attribute__((section(".rodata.sceResident"))) = LIBRARY_NAME;
@@ -40,7 +40,7 @@ static prx_header header __attribute__((section(".lib.stub"))) = {
 
 #define EXPORT(name, fnid) \
 	extern void* __##name; \
-	const void* name##_stub __attribute__((section(".data.sceFStub." LIBRARY_NAME))) = &__##name; \
+	const void* name##_stub __attribute__((section(".data.sceFStub"))) = &__##name; \
 	const uint32_t name##_fnid __attribute__((section(".rodata.sceFNID"))) = fnid;
 
 #include "exports.h"

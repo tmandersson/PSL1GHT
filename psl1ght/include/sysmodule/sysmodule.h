b1c44b926b6a1b4a59dccd7ade32f9b3233a7c78
Scott Mansell <phiren@gmail.com>
2010-11-14 11:30:26 +1300

    32 bit opd hack isn't needed anymore.

--- psl1ght/include/sysmodule/sysmodule.h
+++ psl1ght/include/sysmodule/sysmodule.h
@@ -28,25 +28,6 @@ typedef enum
 	
 } id_module;
 
-// create .opd 32 bits struct from 64 bits .opd addr
-// the actual compilers uses 64 bits pointers but you need 32 bits pointers when one lib call to a remote function
-// note it uses build_opd to alloc the fake .opd entry and return 32 bits addr from build_opd
-
-inline uint32_t build32_func_addr(void *addr, uint64_t *build_opd) {
-	
-	uint64_t *addr_in=(uint64_t *) addr;
-
-	*build_opd= (addr_in[0]<<32ULL) | addr_in[1];
-
-	return (uint32_t) ((uint64_t) build_opd);
-}
-
-inline uint32_t get32_addr(void *addr) {
-	
-	return (uint32_t) (uint64_t) addr;
-}
-
-
 int SysLoadModule(id_module id);
 int SysUnloadModule(id_module id);
 

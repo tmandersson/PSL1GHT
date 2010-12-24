5dc5292fc37e6bcb14e3893263be65a21eddaa55
hermesEOL <hermes.ps2@gmail.com>
2010-12-24 12:34:18 +0100

    Added msgdialog support

--- libpsl1ght/source/libsysutil.c
+++ libpsl1ght/source/libsysutil.c
@@ -1,6 +1,17 @@
 #include "sysutil/events.h"
+#include "sysutil/msgdialog.h"
 
 s32 sysRegisterCallback(sysEventSlot slot, sysCallback func, void * userdata)
 {
 	return sysRegisterCallback_ex(slot, OPD32(func), userdata);
+}
+
+s32 MsgDialogOpen(u32 type, const char * msg, MsgDialog func, void * userdata, void *unused)
+{
+	return MsgDialogOpen_ex(type, msg, OPD32(func), userdata, unused);
+}
+
+s32 MsgDialogErrorCode(u32 errorcode, MsgDialog func, void * userdata, void *unused)
+{
+    return MsgDialogErrorCode_ex(errorcode, OPD32(func), userdata, unused);
 }

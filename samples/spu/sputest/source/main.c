ccedd1b0ad8baf7b875ac924a5d1b21cbcece21c
Miigotu <miigotu@hotmail.com>
2011-03-16 23:01:07 -0400

    Revert the spu samples changes that I added from hermes tarball.

--- samples/spu/sputest/source/main.c
+++ samples/spu/sputest/source/main.c
@@ -12,25 +12,11 @@ int main(int argc, const char* argv[])
 {
 	u32 spu = 0;
 	sysSpuImage image;
-	u32 entry = 0;
-	u32 segmentcount = 0;
-	sysSpuSegment* segments;
 
 	printf("Initializing 6 SPUs... ");
 	printf("%08x\n", lv2SpuInitialize(6, 5));
 	printf("Initializing raw SPU... ");
 	printf("%08x\n", lv2SpuRawCreate(&spu, NULL));
-
-	printf("Getting ELF information... ");
-	printf("%08x\n", sysSpuElfGetInformation(spu_bin, &entry, &segmentcount));
-	printf("\tEntry Point: %08x\n\tSegment Count: %08x\n", entry, segmentcount);
-
-	size_t segmentsize = sizeof(sysSpuSegment) * segmentcount;
-	segments = (sysSpuSegment*)malloc(segmentsize);
-	memset(segments, 0, segmentsize);
-
-	printf("Getting ELF segments... ");
-	printf("%08x\n", sysSpuElfGetSegments(spu_bin, segments, segmentcount));
 
 	printf("Loading ELF image... ");
 	printf("%08x\n", sysSpuImageImport(&image, spu_bin, 0));

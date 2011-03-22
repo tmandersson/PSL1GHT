6d2a8a7feecea82b278fe4d2c3892735e27bd2e2
François Galea <fgalea@free.fr>
2011-03-22 01:11:37 +0100

    Added a main page to the documentation.

---
+++ ppu/psl1ght.h
@@ -0,0 +1,41 @@
+/*! \file psl1ght.h
+ \brief dummy header file for the doxygen documentation.
+
+\mainpage psl1ght-v2 Documentation
+
+\author shagkur
+\author miigotu
+\author zerkman
+
+\section Main topics
+
+Visit the \ref page_install "Installation page".
+
+\section Links
+ - <a href="http://git-hacks.com/devkitpro-addons/devkitps3-psl1ght-v2">
+   psl1ght-v2 repository</a>.
+ - <a href="http://git-hacks.com/devkitpro-addons">devkitpro-addons repository</a>.
+
+*/
+
+
+/*! \page page_install Installation
+
+Set your environment variables this way:
+
+\code
+export DEVKITPRO=/some/where
+export DEVKITPS3=$DEVKITPRO/devkitps3
+\endcode
+
+First, install <a href="http://git-hacks.com/devkitpro-addons/devkitps3-toolchain">
+devkitps3-toolchain</a>.
+
+Then from the main psl1ght-v2 directory, type
+
+\code
+make install
+\endcode
+
+You can build each sample just issuing a \c make command.
+*/

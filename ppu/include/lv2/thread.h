d979fc9e42c9d7635aedc0ca66910ae0b8be1b03
François Galea <fgalea@free.fr>
2011-04-11 23:01:32 +0200

    Added documentation for PPU threads management.

--- ppu/include/lv2/thread.h
+++ ppu/include/lv2/thread.h
@@ -1,3 +1,7 @@
+/*! \file lv2/thread.h
+ \brief PPU thread library.
+*/
+
 #ifndef __LV2_THREAD_H__
 #define __LV2_THREAD_H__
 
@@ -7,9 +11,18 @@
 extern "C" {
 #endif
 
+/*! \brief PPU thread identifier. */
 typedef u64 sys_ppu_thread_t;
 
+/*! \brief Terminate the running PPU thread.
+\param ret_val Thread return value.
+*/
 void sysThreadExit(u64 ret_val);
+
+/*! \brief Get the thread id of the running PPU thread.
+\param threadid Pointer to storage for the running PPU thread id.
+\return zero if no error occured, nonzero otherwise.
+*/
 s32 sysThreadGetId(sys_ppu_thread_t *threadid);
 
 #ifdef __cplusplus

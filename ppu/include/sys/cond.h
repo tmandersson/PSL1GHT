109ddcff4f4fdb0dcfb5183db858fadb6d1189ea
François Galea <fgalea@free.fr>
2011-04-14 08:07:14 +0200

    Added documentation for system condition variables.

--- ppu/include/sys/cond.h
+++ ppu/include/sys/cond.h
@@ -1,4 +1,4 @@
-/*! \file cond.h
+/*! \file sys/cond.h
  \brief Condition variables management.
 */
 
@@ -8,42 +8,76 @@
 #include <mutex.h>
 #include <lv2/cond.h>
 
+/*! \brief Pshared attribute for condition variables. */
 #define SYS_COND_ATTR_PSHARED			0x0200
 
 #ifdef __cplusplus
 	extern "C" {
 #endif
 
-typedef u32 sys_cont_t;
+/*! \brief Condition variable identifier. */
+typedef u32 sys_cond_t;
 
+/*! \brief Condition variable attributes data structure */
 typedef struct sys_cond_attr
 {
+	/*! \brief Pshared flag. Must be 0 or \ref SYS_COND_ATTR_PSHARED. */
 	u32 attr_pshared;
-	s32 flags;
-	u64 key;
-	char name[8];
+	s32 flags;			/*!< Flags. */
+	u64 key;			/*!< Key. */
+	char name[8];		/*!< Name. */
 } sys_cond_attr_t;
 
+/*! \brief Create a condition variable.
+\param cond Pointer to storage for the created condition variable identifier.
+\param mutex Pointer to the associated mutex.
+\param attr Pointer to the attributes data structure.
+\return zero if no error occured, nonzero otherwise.
+*/
 LV2_SYSCALL sysCondCreate(sys_cond_t *cond,sys_mutex_t mutex,const sys_cond_attr_t *attr)
 {
 	return lv2syscall3(105,(u64)cond,mutex,(u64)attr);
 }
 
+/*! \brief Destroy a condition variable.
+\param cond Condition variable identifier.
+\return zero if no error occured, nonzero otherwise.
+*/
 LV2_SYSCALL sysCondDestroy(sys_cond_t cond)
 {
 	return lv2syscall1(106,cond);
 }
 
+/*! \brief Wait for a condition variable to be signaled.
+
+The associated mutex must have been previously locked by the
+calling thread.
+This function atomically unlocks the mutex and waits for the condition variable
+to be signaled.
+Before returning to the calling thread, this function re-acquires the mutex.
+\param cond Condition variable identifier.
+\param timeout_usec Timeout value in microseconds, or 0 if no timeout is used.
+\return zero if the condition variable was signaled,
+nonzero in case of error or if a timeout occured.
+*/
 LV2_SYSCALL sysCondWait(sys_cond_t cond,u64 timeout_usec)
 {
 	return lv2syscall2(107,cond,timeout_usec);
 }
 
+/*! \brief Signal a condition variable to at most one waiting thread.
+\param cond Condition variable identifier.
+\return zero if no error occured, nonzero otherwise.
+*/
 LV2_SYSCALL sysCondSignal(sys_cond_t cond)
 {
 	return lv2syscall1(108,cond);
 }
 
+/*! \brief Signal a condition variable to all waiting threads.
+\param cond Condition variable identifier.
+\return zero if no error occured, nonzero otherwise.
+*/
 LV2_SYSCALL sysCondBroadcast(sys_cond_t cond)
 {
 	return lv2syscall1(109,cond)

c1ea787697881509dcc9cc96b3e41c80b49644a0
shagkur <shagkur@gmx.net>
2011-04-15 16:22:45 +0200

    replaced constants by defines

--- spu/libsputhread/spu_thread_throw_event.c
+++ spu/libsputhread/spu_thread_throw_event.c
@@ -3,12 +3,12 @@
 
 int spu_thread_throw_event(uint8_t spup,uint32_t data0,uint32_t data1)
 {
-	vec_char16 v = si_selb(si_from_uint(0x40000000),si_from_uint(data0),si_fsmbi(0x7777));
+	uint32_t val = (0x40000000 | (spup<<EVENT_PORT_SHIFT) | (data0&EVENT_DATA0_MASK));
 
-	if(spup>63) return 0x80010002;
+	if(spup>EVENT_PORT_MAX_NUM) return 0x80010002;
 
 	spu_writech(SPU_WrOutMbox,data1);
-	spu_writechqw(SPU_WrOutIntrMbox,(v | si_from_uint(spup<<24)));
+	spu_writech(SPU_WrOutIntrMbox,val);
 
 	return 0;
 }

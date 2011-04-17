8a3e5dbdde96262fe5a69e33616b44181ff0048f
shagkur <shagkur@gmx.net>
2011-04-17 09:04:22 +0200

    documented spu_thread_send_event

--- spu/include/sys/spu_event.h
+++ spu/include/sys/spu_event.h
@@ -20,6 +20,20 @@
 extern "C" {
 #endif
 
+/*! Send an SPU thread user event.
+
+This function sends an SPU thread user event to an event queue.
+The sent data will be received as \ref sys_event_t structure type. The data will be stored as the following:
+- source = SPU_THREAD_EVENT_USER_KEY
+- data_1 = SPU thread ID
+- data_2 (upper 32 bits) = SPU port number
+- data_2 (lower 32 bits) = lower 24 bits of <i>data0</i>
+- data_3 = <i>data1</i>
+\param spup SPU port number.
+\param data0 Data 0 (only lower 24 bits are sent)
+\param data1 Data 1
+\return zero if no error occured, nonzero otherwise.
+*/
 int spu_thread_send_event(uint8_t spup,uint32_t data0,uint32_t data1);
 
 int spu_thread_throw_event(uint8_t spup,uint32_t data0,uint32_t data1);

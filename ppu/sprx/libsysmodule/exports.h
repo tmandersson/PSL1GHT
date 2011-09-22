12dea880f722ce8d21c2ec38086f86dba9ff8ef0
an0nym0us <an0nym0us@privacy4.me>
2011-09-21 22:41:12 -0400

    fixed function names

--- ppu/sprx/libsysmodule/exports.h
+++ ppu/sprx/libsysmodule/exports.h
@@ -5,9 +5,9 @@
 EXPORT(sysModuleInitialize, 0x63ff6ff9);
 EXPORT(sysModuleFinalize, 0x96c07adf);
 
-EXPORT(sysModuleLoadEx, 0x32267A31);
-EXPORT(sysModuleUnloadEx, 0x112A5EE9);
-EXPORT(sysModuleIsLoadedEx, 0x5a59e258);
+EXPORT(sysModuleLoad, 0x32267A31);
+EXPORT(sysModuleUnload, 0x112A5EE9);
+EXPORT(sysModuleIsLoaded, 0x5a59e258);
 
 EXPORT(sysModuleGetImagesize, 0x1ef115ef);
 EXPORT(sysModuleFetchImage, 0x3c92be09);
